4beb251fcf37b2c30185ca49a88fadffebbef4e7
Gavin <gavinporlich@gmail.com>
2022-09-22 14:45:50 -0500

    user routes made/session loggedin variable made

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,7 +1,39 @@
+const { Post, Comment, User } = require('../models');
+
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-    
+    try {
+        if (!req.session.loggedIn) {
+            res.redirect('/login');
+        } else {
+            const userPosts = await Post.findAll({
+                include: [
+                    {
+                        model: User,
+                        attributes: ['username'],
+                    },
+                ],
+                raw: true,
+                nest: true,
+            });
+            console.log(userPosts);     
+            res.render('homepage', { 
+                userPosts, 
+                loggedIn: req.session.loggedIn 
+            });
+
+        };
+    } catch(err) {
+        res.status(500).json(err);
+    };
 });
+
+
+
+router.get('/login', async (req, res) => {
+    res.render('logpage', { loggedIn: req.session.loggedIn })
+});
+
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const session = require('express-session');
 const routes = require('./controllers');
+const helpers = require('./utils/helpers')
+
 const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT;
 
-const hbs = exphbs.create();
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: process.env.SESSION_SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess))
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'))
 
 app.use(routes);
 
